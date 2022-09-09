f9b6e36762101cd6b726a450ee3dceeef773d94d
YmerA38 <stin166c@stud.kea.dk>
2022-09-09 13:37:33 +0200

    første

---
+++ src/SuperGreeter.java
@@ -0,0 +1,11 @@
+public class SuperGreeter {
+        public void start(){
+            System.out.println("det virker");
+        }
+        public static void main(String[] args){
+            SuperGreeter sg = new SuperGreeter();
+            sg.start();
+        }
+
+
+}
