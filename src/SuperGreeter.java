b0895b1f98005182b3cebd24a65d742fd1a44041
YmerA38 <stin166c@stud.kea.dk>
2022-09-09 13:39:39 +0200

    første altså kun .java filer

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,6 +1,6 @@
 public class SuperGreeter {
         public void start(){
-            System.out.println("det virker");
+            System.out.println("det virker altså");
         }
         public static void main(String[] args){
             SuperGreeter sg = new SuperGreeter();
