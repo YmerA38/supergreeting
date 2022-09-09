b7fcc07fa38bbe48868c794d71ae9deb7ed9c9b6
YmerA38 <stin166c@stud.kea.dk>
2022-09-09 13:48:06 +0200

    med indtast Navn

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
         public void start(){
             System.out.println("det virker altså");
         }
+        public void indtastNavn(){
+            System.out.println("hvad er dit navn?");
+            Scanner indTast = new Scanner(System.in);
+            String navn = indTast.next();
+            System.out.println("Hej "+navn);
+        }
         public static void main(String[] args){
             SuperGreeter sg = new SuperGreeter();
             sg.start();
+            sg.indtastNavn();
         }
 
 
