bff6d9d04b7a989cfcfef694fc0f270117911768
YmerA38 <stin166c@stud.kea.dk>
2022-09-09 13:55:54 +0200

    med spørg om alder

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -4,16 +4,22 @@ public class SuperGreeter {
         public void start(){
             System.out.println("det virker altså");
         }
+    Scanner indTast = new Scanner(System.in);
         public void indtastNavn(){
             System.out.println("hvad er dit navn?");
-            Scanner indTast = new Scanner(System.in);
             String navn = indTast.next();
             System.out.println("Hej "+navn);
+        }
+        public int dinAlder(){
+            System.out.println("hvor gammel er du?");
+            int alder = indTast.nextInt();
+            return alder;
         }
         public static void main(String[] args){
             SuperGreeter sg = new SuperGreeter();
             sg.start();
             sg.indtastNavn();
+            sg.dinAlder();
         }
 
 
